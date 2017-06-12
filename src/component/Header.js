9308225f90a6057d29fa8d1aaf69018a943cd489
baotonghui <877361655@qq.com>
2017-06-12 18:25:28 +0800

    ll

---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lgq on 2017/6/12.
+ */
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export default class Header extends React.Component{
+    render(){
+        return <h1> react header</h1>
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by lgq on 2017/6/12.
+ */
+import React from 'react';
+import ReactDom from 'react-dom';
+import  Header from './component/Header';
+
+ReactDom.render(<Header/>,document.getElementById('app'));
